e0e9487064528c13f7ae11632d36264eea3dcc28
Jaydeep <160080044+jdpatel1634@users.noreply.github.com>
2025-11-10 22:01:54 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.get("/", (req, res) => res.send("API running..."));
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
 
 import authRoutes from "./routes/auth.js";
 app.use("/api/auth", authRoutes);
