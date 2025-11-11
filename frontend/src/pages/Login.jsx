eeb7ae8b686ccba24068db3e2690baf9f8227c1e
Jaydeep <160080044+jdpatel1634@users.noreply.github.com>
2025-11-10 18:46:28 -0600

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -9,7 +9,7 @@ export default function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("https://your-backend-url/api/auth/login", { email, password });
+      const res = await axios.post("https://campusconnect-2r6u.onrender.com/api/auth/login", { email, password });
       localStorage.setItem("token", res.data.token);
       setMsg("✅ Login successful!");
     } catch {
